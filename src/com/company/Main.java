772d4406f91cbde418070c5ee8b8ac8a57823a65
DelRioV <pablosalvadordelriovergara.sanjose@alumnado.fundacionloyola.net>
2022-04-06 13:57:15 +0200

    Run main parameters example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        printArray(args);
+    }
+
+    private static void printArray(String[] args) {
+        for(String i: args) {
+            System.out.println(i);
+        }
     }
 }
